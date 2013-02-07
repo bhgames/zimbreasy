60981915600aec7747c2219d300a278a50b9dfc1
Jordan Prince <jordanmprince@gmail.com>
2013-02-06 18:05:24 -0600

    C out of RUD for appointments now works with limited options.

--- lib/zimbreasy/mail.rb
+++ lib/zimbreasy/mail.rb
@@ -12,21 +12,21 @@ module Zimbreasy
     #:end_time(opt)
     #:name(opt)
     #:subject(opt)
-    #:content(opt)
+    #:desc(opt)
     #:mime_type(opt)
-    #:organizer(opt)
     def create_appointment(params)
       account.make_call("CreateAppointmentRequest") do |xml|
         xml.CreateAppointmentRequest({ "xmlns" => @zimbra_namespace}) do |xml|
           xml.m({"su" => params[:subject]}) do |xml|
-            xml.content(params[:content]) 
             xml.mp({"ct" =>(params[:mime_type] || "text/plain")})
-            xml.inv({"rsvp" => "1", "compNum" => "0", "method" => "none", "name" => params[:name] }) do |xml|
+            xml.inv({"rsvp" => "1", "compNum" => "0", "method" => "none", "name" => params[:name] }) do |xml| 
+              xml.mp({"ct" =>(params[:mime_type] || "text/plain")})
+              xml.desc(params[:desc]) 
               xml.s({"d" => Zimbreasy.zimbra_date(params[:start_time])}) if params[:start_time]
               xml.e({"d" => Zimbreasy.zimbra_date(params[:end_time])}) if params[:end_time]
             end 
             
-            xml.e({"a" => params[:appointee_email], "t" => "t", "or" => params[:organizer]})
+            xml.e({"a" => params[:appointee_email], "t" => "t"})
           end
         end
       end
