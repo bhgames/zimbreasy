000f23ddae7f68822f25ba489ae509aff06c2fa7
Jordan Prince <jordanmprince@gmail.com>
2013-02-07 16:18:47 -0600

    Added cancel appointment action successfully. Changed create method to return inv_id, not appt_id, and though get still works off appt_id, cancel and modify require inv_id to succeed.

--- lib/zimbreasy/mail.rb
+++ lib/zimbreasy/mail.rb
@@ -15,6 +15,8 @@ module Zimbreasy
     #:subject(opt)
     #:desc(opt)
     #:mime_type(opt)
+    #Returns Appt's Inv id as UID in an i_cal text.. Is normally a number like 140-141,
+    #the first number is the appt id, which you need for getting.
     def create_appointment(params)
       params[:start_time] = Zimbreasy.zimbra_date(params[:start_time]) if params[:start_time]
       params[:end_time] = Zimbreasy.zimbra_date(params[:end_time]) if params[:end_time]
@@ -24,7 +26,7 @@ module Zimbreasy
           appointment_xml_block(xml, params)
         end
       end
-      params.merge!({:appt_id => response.body[:create_appointment_response][:@cal_item_id]})
+      params.merge!({:appt_id => response.body[:create_appointment_response][:@inv_id]})
       make_ical(params)
     end
  
@@ -42,9 +44,9 @@ module Zimbreasy
       calendar.to_ical
     end
 
-    def get_appointment(id)
+    def get_appointment(appt_id)
       response = account.make_call("GetAppointmentRequest") do |xml|
-        xml.GetAppointmentRequest({ "xmlns" => @zimbra_namespace, "id" => id})
+        xml.GetAppointmentRequest({ "xmlns" => @zimbra_namespace, "id" => appt_id})
       end
 
       comp = response[:get_appointment_response][:appt][:inv][:comp]
@@ -54,7 +56,7 @@ module Zimbreasy
         :end_time => comp[:e][:@d], 
         :desc => comp[:desc],  
         :name => comp[:@name],
-        :appt_id => id
+        :appt_id => appt_id
       }
 
       make_ical(hash)
@@ -88,7 +90,7 @@ module Zimbreasy
       appts
     end 
 
-    #same param options as create_appointment, but you can add :appt_id too.
+    #same param options as create_appointment, but you can add :inv_id too.
     def modify_appointment(params)
       params[:start_time] = Zimbreasy.zimbra_date(params[:start_time]) if params[:start_time]
       params[:end_time] = Zimbreasy.zimbra_date(params[:end_time]) if params[:end_time]
@@ -96,13 +98,32 @@ module Zimbreasy
       response = account.make_call("ModifyAppointmentRequest") do |xml|
         xml.ModifyAppointmentRequest({ 
           "xmlns" => @zimbra_namespace, 
-          "id" => "#{params[:appt_id]}-#{params[:appt_id].to_i-1}"
+          "id" => params[:inv_id]
         }) do |xml|
           appointment_xml_block(xml, params)
         end
       end
     
       make_ical(params)
+    end
+
+    #returns true if it worked, inv_id is not appt_id, it's normally something like 320-319, the first number is appt_id.
+    def cancel_appointment(inv_id)
+      unless inv_id and inv_id.is_a?(String) and inv_id.match(/-/) and inv_id.split("-").count==2 #so it has x-y formatting.
+        raise 'inv_id must be string of format x-y, where x and y are numbers.'
+      end
+
+      response = account.make_call("CancelAppointmentRequest") do |xml|
+        xml.CancelAppointmentRequest({ 
+          "xmlns" => @zimbra_namespace, 
+          "id" => inv_id,
+          "comp" => 0
+        }) do |xml|
+          xml.inv({"id" => inv_id, "method" => "none", "compNum" => "0", "rsvp" => "1"})
+        end
+      end
+
+      return !response.body[:cancel_appointment_response].nil?
     end
 
     private
